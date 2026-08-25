95e2b81bc759ea7817df2d2d65de9b13007c4a78
zqcp <ivanrrr76@gmail.com>
2026-08-25 15:08:13 -0700

    Create embedSelectMenuAdd.js

---
+++ src/interactions/messages/embedSelectMenuAdd.js
@@ -0,0 +1,153 @@
+const {
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder
+} = require("discord.js");
+
+const embeds = require("../../embeds/embeds");
+const Embed = require("../../models/Embed");
+
+module.exports = {
+
+    name: "embedSelectMenuAdd",
+
+    type: "button",
+
+    async execute(client, interaction) {
+
+        if (!interaction.guild) {
+            return;
+        }
+
+        const parts =
+            interaction.customId.split(":");
+
+        const name =
+            parts[1];
+
+        const type =
+            parts[2];
+
+        if (!name || !type) {
+            return interaction.reply({
+                embeds: [
+                    embeds.error(
+                        interaction.user,
+                        "I couldn't determine which select menu you're adding."
+                    )
+                ],
+                flags: 64
+            });
+        }
+
+        const saved =
+            await Embed.findOne({
+                guildId: interaction.guild.id,
+                name
+            });
+
+        if (!saved) {
+            return interaction.reply({
+                embeds: [
+                    embeds.error(
+                        interaction.user,
+                        `I couldn't find an embed named **${name}**.`
+                    )
+                ],
+                flags: 64
+            });
+        }
+
+        const types = {
+            role: "Role Select",
+            user: "User Select",
+            channel: "Channel Select",
+            mentionable: "Mentionable Select"
+        };
+
+        if (!types[type]) {
+            return interaction.reply({
+                embeds: [
+                    embeds.error(
+                        interaction.user,
+                        "That select menu type isn't supported."
+                    )
+                ],
+                flags: 64
+            });
+        }
+
+        const modal =
+            new ModalBuilder()
+                .setCustomId(
+                    `embedSelectMenuAddModal:${name}:${type}`
+                )
+                .setTitle(
+                    `Add ${types[type]}`
+                );
+
+        const placeholder =
+            new TextInputBuilder()
+                .setCustomId("placeholder")
+                .setLabel("Placeholder")
+                .setStyle(
+                    TextInputStyle.Short
+                )
+                .setRequired(false)
+                .setPlaceholder(
+                    "Select something..."
+                )
+                .setValue(
+                    "Select something..."
+                );
+
+        const customId =
+            new TextInputBuilder()
+                .setCustomId("customId")
+                .setLabel("Custom ID")
+                .setStyle(
+                    TextInputStyle.Short
+                )
+                .setRequired(false)
+                .setPlaceholder(
+                    "my-select-menu"
+                );
+
+        const disabled =
+            new TextInputBuilder()
+                .setCustomId("disabled")
+                .setLabel("Disabled")
+                .setStyle(
+                    TextInputStyle.Short
+                )
+                .setRequired(false)
+                .setPlaceholder(
+                    "true or false"
+                )
+                .setValue("false");
+
+        modal.addComponents(
+
+            new ActionRowBuilder()
+                .addComponents(
+                    placeholder
+                ),
+
+            new ActionRowBuilder()
+                .addComponents(
+                    customId
+                ),
+
+            new ActionRowBuilder()
+                .addComponents(
+                    disabled
+                )
+
+        );
+
+        return interaction.showModal(modal);
+
+    }
+
+};
