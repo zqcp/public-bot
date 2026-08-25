c9476e4d3ce05bab47758c2c50a78cb813042cf6
zqcp <ivanrrr76@gmail.com>
2026-08-25 00:00:28 -0700

    Update button.js

--- src/interactions/embed/button.js
+++ src/interactions/embed/button.js
@@ -1,11 +1,21 @@
+const {
+    EmbedBuilder
+} = require("discord.js");
+
 module.exports = {
     type: "button",
     name: "embed_",
 
     async execute(interaction) {
 
+        const embed = new EmbedBuilder()
+            .setTitle("Embed Builder")
+            .setDescription(
+                "Use the buttons below to customize your embed."
+            );
+
         await interaction.reply({
-            content: "Embed interaction received.",
+            embeds: [embed],
             flags: 64
         });
 
