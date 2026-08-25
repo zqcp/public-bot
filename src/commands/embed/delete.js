67b15734ae6727242c06939921e999c838e19b51
zqcp <ivanrrr76@gmail.com>
2026-08-25 10:36:30 -0700

    Create delete.js

---
+++ src/commands/embed/delete.js
@@ -0,0 +1,122 @@
+const Embed = require("../../models/Embed");
+const embeds = require("../../embeds/embeds");
+const helpEmbed = require("../../embeds/help/embed");
+const globalEmbeds = require("../../embeds/global");
+
+module.exports = {
+
+    name: "embed delete",
+
+    aliases: [],
+
+    permissions: {
+        user: ["ManageGuild"],
+        bot: ["ManageGuild"]
+    },
+
+    async execute(client, message, args) {
+
+        if (!message.guild) {
+            return;
+        }
+
+        /*
+         * User permission
+         */
+
+        if (
+            !message.member.permissions.has("ManageGuild")
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.permission(
+                        message.author,
+                        "Manage Server"
+                    )
+                ]
+            });
+        }
+
+        /*
+         * Bot permission
+         */
+
+        if (
+            !message.guild.members.me.permissions.has(
+                "ManageGuild"
+            )
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.botPermission(
+                        message.author,
+                        ["ManageGuild"]
+                    )
+                ]
+            });
+        }
+
+        /*
+         * Help
+         */
+
+        const name = args
+            .join(" ")
+            .trim()
+            .toLowerCase();
+
+        if (!name) {
+            return message.channel.send({
+                embeds: [
+                    helpEmbed.delete(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        /*
+         * Find saved embed
+         */
+
+        const existing =
+            await Embed.findOne({
+                guildId: message.guild.id,
+                name
+            });
+
+        if (!existing) {
+            return message.channel.send({
+                embeds: [
+                    embeds.error(
+                        message.author,
+                        `I couldn't find an embed named **${name}**.`
+                    )
+                ]
+            });
+        }
+
+        /*
+         * Delete the saved configuration.
+         *
+         * This intentionally removes the configuration
+         * and its stored message references.
+         */
+
+        await Embed.deleteOne({
+            guildId: message.guild.id,
+            name
+        });
+
+        return message.channel.send({
+            embeds: [
+                embeds.success(
+                    message.author,
+                    `Embed **${name}** has been deleted.`
+                )
+            ]
+        });
+
+    }
+
+};
