fbe225e2c6edc675bbc49feedf9d801a7e2dd463
zqcp <ivanrrr76@gmail.com>
2026-08-25 10:17:08 -0700

    Create Embed.js

---
+++ src/models/Embed.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const EmbedSchema = new mongoose.Schema(
+    {
+        guildId: {
+            type: String,
+            required: true,
+            index: true
+        },
+
+        name: {
+            type: String,
+            required: true
+        },
+
+        content: {
+            type: String,
+            default: null
+        },
+
+        embeds: {
+            type: mongoose.Schema.Types.Mixed,
+            default: []
+        },
+
+        components: {
+            type: mongoose.Schema.Types.Mixed,
+            default: []
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+EmbedSchema.index(
+    {
+        guildId: 1,
+        name: 1
+    },
+    {
+        unique: true
+    }
+);
+
+module.exports =
+    mongoose.models.Embed ||
+    mongoose.model(
+        "Embed",
+        EmbedSchema
+    );
