a66278e27cc047871dd6ae0925b4343e1c513528
zqcp <ivanrrr76@gmail.com>
2026-08-27 11:47:34 -0700

    Create filter.js

---
+++ src/events/filter.js
@@ -0,0 +1,47 @@
+// src/events/filter.js
+
+const CommandHandler =
+    require("../handlers/commandHandler");
+
+const TextFilter =
+    require("../systems/textFilter");
+
+module.exports = {
+
+    name: "messageCreate",
+
+    async execute(
+        client,
+        message
+    ) {
+
+        /*
+         * Text filter
+         */
+
+        const filtered =
+            await TextFilter.handle(
+                client,
+                message
+            );
+
+        /*
+         * Message was filtered
+         */
+
+        if (filtered) {
+            return;
+        }
+
+        /*
+         * Command handler
+         */
+
+        await CommandHandler.handle(
+            client,
+            message
+        );
+
+    }
+
+};
