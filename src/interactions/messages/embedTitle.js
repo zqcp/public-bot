f4aa651f3687848541d49ca2b633f5e50e9ad5fb
zqcp <ivanrrr76@gmail.com>
2026-08-27 00:43:21 -0700

    Create     embedTitle.js

---
+++ src/interactions/messages/embedTitle.js
@@ -0,0 +1,91 @@
+const {
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ActionRowBuilder
+} = require("discord.js");
+
+const Embed = require("../../models/Embed");
+const embeds = require("../../embeds/embeds");
+
+module.exports = {
+
+    name: "embedTitle",
+
+    type: "button",
+
+    async execute(client, interaction) {
+
+        if (!interaction.guild) {
+            return;
+        }
+
+        const [, name] =
+            interaction.customId.split(":");
+
+        if (!name) {
+            return interaction.reply({
+                embeds: [
+                    embeds.error(
+                        interaction.user,
+                        "I couldn't determine which embed you're editing."
+                    )
+                ],
+                flags: 64
+            });
+        }
+
+        const saved =
+            await Embed.findOne({
+                guildId: interaction.guild.id,
+                name
+            });
+
+        if (!saved) {
+            return interaction.reply({
+                embeds: [
+                    embeds.error(
+                        interaction.user,
+                        `I couldn't find an embed named **${name}**.`
+                    )
+                ],
+                flags: 64
+            });
+        }
+
+        const currentTitle =
+            Array.isArray(saved.embeds) &&
+            saved.embeds[0]?.title
+                ? String(saved.embeds[0].title)
+                : "";
+
+        const input =
+            new TextInputBuilder()
+                .setCustomId("title")
+                .setLabel("Title")
+                .setStyle(
+                    TextInputStyle.Short
+                )
+                .setRequired(false)
+                .setMaxLength(256)
+                .setValue(
+                    currentTitle
+                );
+
+        const modal =
+            new ModalBuilder()
+                .setCustomId(
+                    `embedTitleModal:${name}`
+                )
+                .setTitle("Edit Title");
+
+        modal.addComponents(
+            new ActionRowBuilder()
+                .addComponents(input)
+        );
+
+        return interaction.showModal(modal);
+
+    }
+
+};
