154da5909103dbb900da35d67b3830a0d9711bca
zqcp <ivanrrr76@gmail.com>
2026-08-29 13:32:45 -0700

    Update select.js

--- src/interactions/messages/role/select.js
+++ src/interactions/messages/role/select.js
@@ -1,3 +1,9 @@
+const {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require("discord.js");
+
 module.exports = {
 
     name: "roleSelect",
@@ -22,19 +28,75 @@ module.exports = {
             );
 
         if (!role) {
-
             return interaction.reply({
                 content:
                     "That role no longer exists.",
                 flags: 64
             });
-
         }
 
+        const buttons =
+            new ActionRowBuilder()
+                .addComponents(
+
+                    new ButtonBuilder()
+                        .setCustomId(
+                            `roleAdd:${role.id}`
+                        )
+                        .setLabel("Add")
+                        .setStyle(
+                            ButtonStyle.Secondary
+                        ),
+
+                    new ButtonBuilder()
+                        .setCustomId(
+                            `roleEdit:${role.id}`
+                        )
+                        .setLabel("Edit")
+                        .setStyle(
+                            ButtonStyle.Secondary
+                        ),
+
+                    new ButtonBuilder()
+                        .setCustomId(
+                            `roleRemove:${role.id}`
+                        )
+                        .setLabel("Remove")
+                        .setStyle(
+                            ButtonStyle.Secondary
+                        ),
+
+                    new ButtonBuilder()
+                        .setCustomId(
+                            `roleMove:${role.id}`
+                        )
+                        .setLabel("Move")
+                        .setStyle(
+                            ButtonStyle.Secondary
+                        ),
+
+                    new ButtonBuilder()
+                        .setCustomId(
+                            `roleSave:${role.id}`
+                        )
+                        .setLabel("Save")
+                        .setStyle(
+                            ButtonStyle.Success
+                        )
+
+                );
+
         return interaction.reply({
+
             content:
                 `Selected **${role.name}**\nRole ID: \`${role.id}\``,
+
+            components: [
+                buttons
+            ],
+
             flags: 64
+
         });
 
     }
