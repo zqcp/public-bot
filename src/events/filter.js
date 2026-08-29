bd53dd74009ea2aabe0e25c9b8ff667328803350
zqcp <ivanrrr76@gmail.com>
2026-08-29 03:43:13 -0700

    Update filter.js

--- src/events/filter.js
+++ src/events/filter.js
@@ -1,22 +1,36 @@
 // src/events/filter.js
-
 const TextFilter =
     require("../systems/TextFilter");
-
 module.exports = {
-
     name: "messageCreate",
-
     async execute(
         message,
         client
     ) {
-
-        await TextFilter.handle(
-            client,
-            message
-        );
-
+        /*
+         * Ignore invalid messages.
+         */
+        if (
+            !message ||
+            !message.guild ||
+            !message.author ||
+            message.author.bot
+        ) {
+            return;
+        }
+        /*
+         * Run text filter.
+         */
+        try {
+            await TextFilter.handle(
+                client,
+                message
+            );
+        } catch (error) {
+            console.error(
+                "[FILTER EVENT] Failed to process message:",
+                error
+            );
+        }
     }
-
 };
