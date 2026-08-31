dd51b379bbeac3561da17f785a787360e7866aac
zqcp <ivanrrr76@gmail.com>
2026-08-31 10:42:54 -0700

    Update kick.js

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -249,19 +249,23 @@ module.exports = {
             await target.kick(reason);
 
             /*
-             * Success embed
+             * Success
              */
 
-            const embed =
-                new EmbedBuilder()
-                    .setColor(config.colors.success)
-                    .setDescription(
-                        `${config.emojis.success} ${message.author}: Kicked **${target.user.username}** for ${reason}.`
-                    );
+            if (
+                reason ===
+                "No reason provided"
+            ) {
 
-            return message.channel.send({
-                embeds: [embed]
-            });
+                return message.channel.send(
+                    `kicked ${target}`
+                );
+
+            }
+
+            return message.channel.send(
+                `kicked ${target} for \`${reason}\``
+            );
 
         } catch (error) {
 
@@ -276,7 +280,9 @@ module.exports = {
 
             const embed =
                 new EmbedBuilder()
-                    .setColor(config.colors.failed)
+                    .setColor(
+                        config.colors.failed
+                    )
                     .setDescription(
                         `${config.emojis.failed} ${message.author}: Kick failed for **${target.user.username}**. Please try again.`
                     );
