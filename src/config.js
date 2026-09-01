40ba2c3d95e339144fc328c1f94b2e2644c9673b
zqcp <ivanrrr76@gmail.com>
2026-09-01 02:49:25 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,30 @@
 module.exports = {
 
-    prefix: ".",
+    prefix: ",",
 
     colors: {
-        regular: 0x000000,
-        success: 0x57F288,
+        regular: 0x3D3D45,
+        error: 0xFAA819,
+        role: 0x31BBDD,
+        success: 0xA9EB7F,
         failed: 0xFF6763
     },
 
     emojis: {
-        success: "✅",
-        failed: "❌"
+        success: "<:success:1541970152814551162>",
+        failed: "<:failed:1541973642345185280>",
+        error: "<:error:1541970187786653797>",
+
+        // Command specific
+        add: "<:add:1541970245453873313>",
+        remove: "<:remove:1541970285715132416>"
     },
 
     status: {
         enabled: true,
         text: "",
         type: 0,          // 0=Playing, 1=Streaming, 2=Listening, 3=Watching, 5=Competing
-        status: "dnd",    // online, idle, dnd, invisible
+        status: "dnd", // online, idle, dnd, invisible
         url: ""           // Only needed if type is 1 (Streaming)
     }
 
