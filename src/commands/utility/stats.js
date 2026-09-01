8f5998dc8991ce4b7c6de2d1a8ebf44d3806821a
zqcp <ivanrrr76@gmail.com>
2026-09-01 12:42:49 -0700

    Create stats.js

---
+++ src/commands/utility/stats.js
@@ -0,0 +1,86 @@
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const config =
+    require("../../config");
+
+const VCSystem =
+    require("../../systems/voiceLevel");
+
+module.exports = {
+
+    name: "stats",
+
+    async execute(client, message) {
+
+        const member =
+            message.member;
+
+        const stats =
+            await VCSystem.getStats(
+                message.guild.id,
+                member.id
+            );
+
+        const tier =
+            await VCSystem.getTier(
+                message.guild.id,
+                stats.allTime
+            );
+
+        const embed =
+            new EmbedBuilder()
+
+                .setTitle(
+                    `${member.user.username}'s VC Stats`
+                )
+
+                .setThumbnail(
+                    member.user.displayAvatarURL({
+                        dynamic: true,
+                        size: 256
+                    })
+                )
+
+                .setDescription(
+                    "This only counts if you are **unmuted** and **undeafened**."
+                )
+
+                .addFields(
+                    {
+                        name:
+                            "Current Tier",
+
+                        value:
+                            `> ${tier.name}`,
+
+                        inline:
+                            false
+                    },
+                    {
+                        name:
+                            "\u200b",
+
+                        value:
+                            `**All Time**: ${VCSystem.formatTime(stats.allTime)}\n` +
+                            `**Today**: ${VCSystem.formatTime(stats.today)}`,
+
+                        inline:
+                            false
+                    }
+                )
+
+                .setColor(
+                    config.colors.regular
+                );
+
+        return message.reply({
+            embeds: [
+                embed
+            ]
+        });
+
+    }
+
+};
