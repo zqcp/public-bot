2113bf34fda28cbbd516621ae1b25c80fa4e9f28
zqcp <ivanrrr76@gmail.com>
2026-09-02 00:59:45 -0700

    Create voiceLevel.js

---
+++ src/events/voiceLevel.js
@@ -0,0 +1,53 @@
+// src/events/voiceLevel.js
+
+const {
+    EmbedBuilder
+} = require("discord.js");
+
+const config =
+    require("../config");
+
+
+module.exports = {
+
+    levelUp(
+        user,
+        level,
+        roleName = null
+    ) {
+
+        return new EmbedBuilder()
+
+            .setColor(
+                config.colors.regular
+            )
+
+            .setAuthor({
+                name:
+                    user.username,
+
+                iconURL:
+                    user.displayAvatarURL({
+                        dynamic: true
+                    })
+            })
+
+            .setThumbnail(
+                user.displayAvatarURL({
+                    dynamic: true,
+                    size: 256
+                })
+            )
+
+            .setDescription(
+                `${config.emojis.success} Congrats ${user}, you reached voice level \`${level}\`!` +
+                (
+                    roleName
+                        ? `\n\nYou earned the **${roleName}** rank!`
+                        : ""
+                )
+            );
+
+    }
+
+};
